0bbb1bed300b68bf76b7a73f3146c6a94e4f8163
rob <rob.ludwig@rideamigos.com>
2017-09-08 16:14:35 -0400

    models & project files

---
+++ backend/models/Challenge.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+let Challenge = {
+  name : {type : String, required : true},
+  description : {type : String, required : true},
+
+  //only triplogs after the startDate and before the endDate are eligible
+  startDate : {type: Date, required : true},
+  endDate : {type : Date, required : true},
+
+  //only triplogs with the given mode are eligible
+  mode : {type : String, enum : ["bike", "walk", "carpool"]}
+}
+
+let ChallengeSchema = new mongoose.Schema(Challenge);
+
+//example of how to do a model method in mongoose
+ChallengeSchema.methods.formattedName = function(callback){
+  const fullName = this.name + ": A " + this.mode + " challenge.";
+  return process.nextTick(() => callback(null, fullName));
+}
+
+module.exports = mongoose.model("Challenge", ChallengeSchema);
---
+++ backend/models/TripLog.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+let TripLog = {
+  userId : {type : mongoose.Schema.ObjectId},
+  miles : {type : Number, required: true},
+  timestamp : {type : Date, required : true},
+  mode : {type : String, enum : ["bike", "walk", "carpool", "drive", "bus"]}
+}
+
+let TripLogSchema = new mongoose.Schema(TripLog);
+
+//example of how to do a model method in mongoose
+TripLogSchema.methods.humanDescription = function(callback){
+  const description = "User " + this.userId + " " + this.mode + "ed on " + this.date;
+  return process.nextTick(() => callback(null, description));
+}
+
+module.exports = mongoose.model("TripLog", TripLogSchema);
