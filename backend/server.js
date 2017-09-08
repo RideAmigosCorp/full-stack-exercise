355a08c042c5c86b17f7e47d2c0e56610bdf37ab
rob <rob.ludwig@rideamigos.com>
2017-09-08 16:47:35 -0400

    backend skeleton largely complete

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const port = process.env.PORT || 3000;
+const mongoUrl = process.env.MONGO || "mongodb://localhost";
+
+//connect
+mongoose.connect(mongoUrl, function(err){
+  if(err) console.error("error connecting to mongodb. things might head south");
+  else console.log("connected to mongo!");
+});
+
+let challengeRoutes = require("./routes/challenges");
+let triplogRoutes = require("./routes/triplogs");
+
+let app = express();
+
+//mount at the specified point. so a route in the challenges routes file that
+//looks like router.get("/hello") will be available at "/challenges/hello
+app.use("/challenges", challengeRoutes);
+app.use("/triplogs", triplogRoutes);
+
+//basic route to see if things are working
+app.get("/", (req, res) => res.end("Hello World!"));
+
+
+app.listen(port, () => console.log("listening on port %d", port));
